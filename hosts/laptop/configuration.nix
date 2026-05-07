06d8fd1e81780ae8758049761ae227c5de0e5f9a
Irek <irrk@op.pl>
2026-05-07 22:32:37 +0200

    feat: Add global python311, gcc, runtimeLibs and environment variables

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,6 +4,30 @@
 
 { config, pkgs, lib, self, ... }:
 
+let
+  runtimeLibs = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    glib
+    dbus
+    fontconfig
+    freetype
+    libGL
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libxcb
+    xorg.xcbutil
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+    xorg.xcbutilwm
+    libxkbcommon
+    wayland
+  ];
+in
+
+
 {
   imports = [
       # Include the results of the hardware scan.
@@ -13,9 +37,16 @@
       ../../modules/services/default.nix
     ]; # Add semicolon here
 
+  environment.systemPackages = with pkgs; [
+    python311
+    gcc
+  ] ++ runtimeLibs;
 
+  environment.extraInit = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
+  '';
 
-
-
-
+  environment.sessionVariables = {
+    QT_QPA_PLATFORM = "offscreen";
+  };
 }
